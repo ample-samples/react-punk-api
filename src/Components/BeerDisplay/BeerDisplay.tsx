f22ff14381ce4062202f7149f93f6b874d8b1f99
Todd <todd.griffin61@outlook.com>
2024-04-02 12:22:46 +0100

    using complete punkapi data set and added key to beer card array result

--- src/Components/BeerDisplay/BeerDisplay.tsx
+++ src/Components/BeerDisplay/BeerDisplay.tsx
@@ -1,12 +1,12 @@
 import BeerCard from "../BeerCard/BeerCard"
-import beers from "../../data/beers"
+import beers from "../../data/data_formatted"
 import "./BeerDisplay.scss"
 
 const BeerDisplay = () => {
   return (
       <section className="beer-display">
-        {beers.map(beer => {
-          return <BeerCard beer={beer} />
+        {beers.map((beer, index) => {
+          return <BeerCard beer={beer} key={index} />
         })}
       </section>
   )
