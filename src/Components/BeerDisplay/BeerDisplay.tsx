e3cb94a9476447f36f1a486e354d738c896b1f23
Todd <todd.griffin61@outlook.com>
2024-04-03 10:18:51 +0100

    search filter implemented

--- src/Components/BeerDisplay/BeerDisplay.tsx
+++ src/Components/BeerDisplay/BeerDisplay.tsx
@@ -7,10 +7,20 @@ type BeerDisplayProps = {
 	searchTerm: string;
 }
 
+const filterMatchesName = (searchItem: string, userSearch: string) => {
+  if (userSearch === '') {
+    return true
+  } else if (searchItem.toLowerCase().includes(userSearch.toLowerCase())) {
+    return true
+  }
+  return false
+}
+
 const BeerDisplay = ({ filter, searchTerm }: BeerDisplayProps) => {
   return (
       <section className="beer-display">
         {beers.map((beer, index) => {
+        if (!filterMatchesName(beer.name, searchTerm)) return
           return <BeerCard beer={beer} key={index} />
         })}
       </section>
