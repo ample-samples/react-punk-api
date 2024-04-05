86f00e22b5dc289f90686a0dbe6459e16d680bf3
Todd <todd.griffin61@outlook.com>
2024-04-05 12:01:19 +0100

    added tests for beer display behaviour

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react"
 import App from "./App"
 import BeerDisplay from "./Components/BeerDisplay/BeerDisplay"
 
-describe('rendering components at a high level', () => {
+describe("rendering components at a high level", () => {
   it("should load the beer display", () => {
     render(<App />);
     const beerDisplay = screen.getByTestId("beer-display");
@@ -20,4 +20,25 @@ describe('rendering components at a high level', () => {
   })
 })
 
+describe("beer display behaviour", () => {
+  it("should show many beers on first load", () => {
+    render(<BeerDisplay filter={{}} searchTerm="" />)
+    const beerDisplay = screen.getByTestId("beer-display");
 
+    expect(beerDisplay.childNodes.length > 5).toBeTruthy()
+  })
+
+  it("should show no beers if light beers and dark beers are filtered for", () => {
+    render(<BeerDisplay filter={{"dark-beer": true, "light-beer": true}} searchTerm="" />)
+    const beerDisplay = screen.getByTestId("beer-display");
+
+    expect(beerDisplay.childNodes.length === 0).toBeTruthy()
+  })
+
+  it("should show no beers if the user searches for gibberish", () => {
+    render(<BeerDisplay filter={{}} searchTerm="1lk2jhkj0p214h321jkp" />)
+    const beerDisplay = screen.getByTestId("beer-display");
+
+    expect(beerDisplay.childNodes.length === 0).toBeTruthy()
+  })
+})
