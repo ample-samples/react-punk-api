41c23d9c804115ff3a160ffc17aac9f9e3108802
Todd <todd.griffin61@outlook.com>
2024-04-05 14:16:16 +0100

    added sidebar toggle for mobile view

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import SearchBar from "../SearchBar/SearchBar"
 import FilterSelection from "../FilterSelection/FilterSelection"
-import { ChangeEvent, FormEventHandler } from "react"
+import { ChangeEvent, FormEventHandler, useState } from "react"
 import "./Sidebar.scss"
 
 type SideBarProps = {
@@ -9,15 +9,36 @@ type SideBarProps = {
 }
 
 const Sidebar = ({ handleSearch, handleFilter }: SideBarProps) => {
+  const [sidebarIsHidden, setSidebarIsHidden] = useState(true)
+
+  const handleClick = () => {
+    setSidebarIsHidden(() => !sidebarIsHidden)
+  }
+
   return (
-    <section data-testid="sidebar" className="sidebar">
-      <div className="sidebar__logo">
-        <img src="https://icons.iconarchive.com/icons/iconarchive/fat-sugar-food/256/Drink-Beer-icon.png" alt="" />
-	<p>Beer Buddy</p>
+    <>
+      <div className="sidebar-toggle">
+        <img  onClick={handleClick} src="https://www.svgrepo.com/show/506800/burger-menu.svg" alt="" />
+	{ !sidebarIsHidden &&
+	  <section data-testid="sidebar-small" className="sidebar-small">
+	    <div className="sidebar__logo">
+	      <img src="https://icons.iconarchive.com/icons/iconarchive/fat-sugar-food/256/Drink-Beer-icon.png" alt="" />
+	      <p>Beer Buddy</p>
+	    </div>
+	    <SearchBar handleSearch={handleSearch} />
+	    <FilterSelection handleFilter={handleFilter} />
+	  </section>
+	}
       </div>
-      <SearchBar handleSearch={handleSearch} />
-      <FilterSelection handleFilter={handleFilter} />
-    </section>
+      <section data-testid="sidebar" className="sidebar">
+        <div className="sidebar__logo">
+          <img src="https://icons.iconarchive.com/icons/iconarchive/fat-sugar-food/256/Drink-Beer-icon.png" alt="" />
+          <p>Beer Buddy</p>
+        </div>
+        <SearchBar handleSearch={handleSearch} />
+        <FilterSelection handleFilter={handleFilter} />
+      </section>
+    </>
   )
 }
 
